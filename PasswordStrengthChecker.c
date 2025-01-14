d03303f876ff21a3976404fb7fc158cf5a42ac02
Kajal Pawar <kp668138@gmail.com>
2025-01-14 17:21:31 +0530

    Declare all variables and get all details from user

---
+++ PasswordStrengthChecker.c
@@ -0,0 +1,28 @@
+//if-else,loop,string,array,File handling this concepts are used in this project.
+#include<stdio.h>
+#include<string.h>
+int main()
+{	
+
+    //Declaration of all variables
+	char name[35],mob[10],email[20],birthdate[8],pass[20];
+	int upr=0,lwr=0,digit=0,splchar=0,flag=0;
+	int i,j,length;
+	char ch1,ch2;
+	
+	//All details of user	
+	printf("Enter your name:");
+	fgets(name,30,stdin);
+	printf("Enter your Mobile number:");
+	scanf("%s",mob);
+	printf("Enter your email:");
+    scanf("%s",email);
+	printf("Enter your birthdate:");
+	scanf("%s",birthdate);
+	printf("Enter Password:");
+	scanf("%s",pass);
+	
+	
+}
+
+
